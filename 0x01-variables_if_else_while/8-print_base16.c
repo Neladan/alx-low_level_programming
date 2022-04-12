0021b9e702a00c302fabe4e687341de128c44eaa
Lionel Adankon <lionelad006@gmail.com>
2022-04-11 18:18:00 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char x,
+
+    for(x = '0'; x <= '9'; ++x)
+      putchar(x);
+  for(x = 'a'; x <= 'f'; ++x)
+    putchar(x);
+
+  putchar('\n');
+
+  return (0);
+}
