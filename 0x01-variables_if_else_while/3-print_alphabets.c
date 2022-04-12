6a260ffb9a21d35290e143f3e71c3b446ff58770
Lionel Adankon <lionelad006@gmail.com>
2022-04-11 17:41:51 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+  int c;
+
+  for (c = 0; c < 26; c++)
+    putchar('a' +c);
+  for (c = 0; c < 26; ++c)
+    putchar('A' + c);
+
+  putchar('\n');
+
+  return (0);
+}
